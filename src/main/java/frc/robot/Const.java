ceab024aee5b4a6a666051d36f602f71375e07bd
jrgalvez01 <jesusrgalvez01@gmail.com>
2020-03-02 13:40:34 -0500

    Moved k constants to own file

---
+++ src/main/java/frc/robot/Const.java
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * All k = constants used located here.  Just to clean up the code a bit.
+ */
+public class Const {
+    // drive motor controller can id's
+    public static final int kFrontLeftChannel = 5;
+    public static final int kRearLeftChannel = 3;
+    public static final int kFrontRightChannel = 6;
+    public static final int kRearRightChannel = 2;
+
+  // shooter motor controller can id's
+    public static final int kLeftShooterID = 1;
+    public static final int kRightShooterID = 7;
+
+  // lift motor controller can id
+    public static final int kLiftMotorID = 9;
+
+  // collector motor pwm id 
+    public static final int kCollectorPWMPort = 8;
+
+    public static final int kPCMCanID = 15;
+    public static final int kCollectorForwardPort = 0;
+    public static final int kCollectorBackwardPort = 1;
+
+    public static final int kDriverXBoxPort = 0;
+    public static final int kOperXBoxPort = 1;
+
+}
